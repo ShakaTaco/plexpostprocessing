c81f55af0ddf5614ae1558cd3a2b8aa849cacc0f
ShakaTaco <193465438+ShakaTaco@users.noreply.github.com>
2025-01-14 15:03:31 -0800

    Update plexpostprocessing.sh
    Cleaned up the script, removed unnecessary comments, added ability to transcode multiple files at a time by running manually and adding additional files as arguments via terminal.

--- plexpostprocessing.sh
+++ plexpostprocessing.sh
@@ -15,66 +15,6 @@
 # ---------------------------------------------------------
 
 # ---------------------------------------------------------
-# Notes:
-# ---------------------------------------------------------
-# Name the script plexpostprocessing.sh and chmod +x on it.
-# Then save it here: var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scripts.
-# Make sure to change the location of the comskip.ini file, list the location below in the default
-# configurations, chmod 777 on it as well if needed so the Plex user can call it.  (This is currently commented out for
-# comcut until it can call the correct file location.  Until then, it uses /root/.comskip.ini which is apparently created when
-# git cloning the respository, installing one of the apps, and then after the first initial run.  Check /root/.comskip.ini
-# exists after installing all required tools and running once.  Change log directory, tmp directory, compskip.ini directory
-# and comchap/comcut installation location as necessary.  Commented out use of all processors for HandBrake and instead using
-# half.  Change the number of processors for HandBrake as needed.  This is done via --encopts="threads=$(( $(nproc) / 2 ))".
-# This uses nproc (the number of processors on the server) divided by 2.  This script can be adjusted at the end to process
-# multiple files via arguments in terminal.  One could also update this to run on any .ts files found in the DVR/recordings
-# folder.  This script can be used for Plex or Jellyfin.  Perhaps Emby or others as well with some adjustment.
-# Seeing an issue with 1080p files creating an .edl file and removing commercials.  Could be local to my setup.
-#
-# Comskip.ini examples: 
-# https://discussion.mcebuddy2x.com/t/comskip-ini-help/4353
-# https://www.kaashoek.com/comskip/viewforum.php?f=7&sid=a009d7f9b6236e73953d2a625b1062d2
-#
-# ---------------------------------------------------------
-# Future Adjustment Ideas:
-# ---------------------------------------------------------
-# Chapter creation for Plex use
-# Automatic file renaming
-# Another option for extracting and embedding subtitles
-# More/Better error handling and logging, if necessary
-# Update to work with more than .ts files
-# Test output to formats other than .mp4
-# Review mkvpropedit, mkvtoolnix, mkvmerge, comskip, ccextractor, etc. for expanded use cases
-# Multithread and parallel processing
-# Disk space check
-# Progress reporting when run manually
-# Email alerts
-# Input validation
-# Processing for all .ts files in folder
-# 1080p .edl file creation and commercial removing fix (corrupted double-linked list)
-
-# ---------------------------------------------------------
-# Required tools for this script:
-# 1. **HandBrakeCLI**: Used for transcoding the .ts file into the desired format.
-#    - Installation: https://handbrake.fr/downloads.php
-#    - On Ubuntu:
-#      sudo apt install handbrake handbrake-cli
-# 2. **ffprobe**: Used to get the resolution of the video file.
-#    - Installation: https://ffmpeg.org/download.html
-#    - On Ubuntu:
-#      sudo apt install ffmpeg
-# 3. **comchap**: Used to add chapters to the transcoded file.
-#    - Installation: https://github.com/BrettSheleski/comchap
-#    - git clone https://github.com/BrettSheleski/comchap
-# 4. **comcut**: Optional tool for cutting commercials from the video.
-#    - Installation: https://github.com/BrettSheleski/comchap
-#    - git clone https://github.com/BrettSheleski/comchap
-# 5. **git**: Use to clone git repositories.
-#    - Installation on Ubuntu:
-#      sudo apt install git											
-# ---------------------------------------------------------
-
-# ---------------------------------------------------------
 # Default configurations
 # ---------------------------------------------------------
 LOG_FILE="/PATH/TO/LOCATION"	# Log for processing output
@@ -233,8 +173,6 @@ process_file() {
     local transcoded_file="$TMP_DIR/$(basename "$input_file" .ts).$OUTPUT_FORMAT"
     log_message "Transcoding with HandBrakeCLI. Output: $transcoded_file"
     HandBrakeCLI -i "$TMP_DIR/$(basename "$input_file")" -o "$transcoded_file" -e x264 -q 20 -B 160 -s "$SUBTITLE_TRACKS" --preset="$HANDBRAKE_PRESET" --encopts="threads=$(( $(nproc) / 2 ))"
-	#HandBrakeCLI -i "$TMP_DIR/$(basename "$input_file")" -o "$transcoded_file" -e x264 -q 20 -B 160 -s "$SUBTITLE_TRACKS" --preset="$HANDBRAKE_PRESET" --encopts="threads=$(nproc)"
-
 
     if [ $? -ne 0 ]; then
         log_message "Error: HandBrakeCLI failed for '$input_file'."
@@ -269,113 +207,10 @@ process_file() {
 # ---------------------------------------------------------
 main() {
     check_dependencies
-    validate_input "$@"
+    for input_file in "$@"; do
+    validate_input "$input_file"
     process_file "$input_file"
+done
 }
 
 main "$@"
-
-# ---------------------------------------------------------
-# Script entry point v2 for multi-file arguments via command line.  Just comment out the script entry point above
-# ---------------------------------------------------------
-#main() {
-#    check_dependencies
-#    for input_file in "$@"; do
-#    validate_input "$input_file"
-#    process_file "$input_file"
-#done
-#}
-
-#main "$@"
-
-# HandBrake Presets - choose from the following options below and hardcode it above to change the preset being used.
-#
-# Preset Type|Preset Name|Type|Video|Audio|Picture Quality|Encoding Speed|File Size|Max Resolution|Frame Types|GOP Size
-# General|Very Fast 2160p60 4K AV1|MP4|AV1|AAC stereo|Average|Very fast|Small|||
-# General|Very Fast 2160p60 4K HEVC|MP4|H.265|AAC stereo|Average|Very fast|Small|||
-# General|Very Fast 1080p30|MP4|H.264|AAC stereo|Average|Very fast|Small|||
-# General|Very Fast 720p30|MP4|H.264|AAC stereo|Average|Very fast|Small|||
-# General|Very Fast 576p25|MP4|H.264|AAC stereo|Average|Very fast|Small|||
-# General|Very Fast 480p30|MP4|H.264|AAC stereo|Average|Very fast|Small|||
-# General|Fast 2160p60 4K AV1|MP4|AV1|AAC stereo|Standard|Fast|Average|||
-# General|Fast 2160p60 4K HEVC|MP4|H.265|AAC stereo|Standard|Fast|Average|||
-# General|Fast 1080p30|MP4|H.264|AAC stereo|Standard|Fast|Average|||
-# General|Fast 720p30|MP4|H.264|AAC stereo|Standard|Fast|Average|||
-# General|Fast 576p25|MP4|H.264|AAC stereo|Standard|Fast|Average|||
-# General|Fast 480p30|MP4|H.264|AAC stereo|Standard|Fast|Average|||
-# General|HQ 2160p60 4K AV1 Surround|MP4|AV1|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|HQ 2160p60 4K HEVC Surround|MP4|H.265|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|HQ 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|HQ 720p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|HQ 576p25 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|HQ 480p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|High|Slow|Large|||
-# General|Super HQ 2160p60 4K AV1 Surround|MP4|AV1|AAC stereo; Dolby Digital (AC-3)|Super High|Very slow|Very large|||
-# General|Super HQ 2160p60 4K HEVC Surround|MP4|H.265|AAC stereo; Dolby Digital (AC-3)|Super High|Very slow|Very large|||
-# General|Super HQ 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|Super high|Very slow|Very large|||
-# General|Super HQ 720p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|Super high|Very slow|Very large|||
-# General|Super HQ 576p25 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|Super high|Very slow|Very large|||
-# General|Super HQ 480p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)|Super high|Very slow|Very large|||
-# Web|Creator 2160p60 4K|MP4|H.264|AAC stereo|High|Medium|Large|||
-# Web|Creator 1440p60 2.5K|MP4|H.264|AAC stereo|High|Medium|Large|||
-# Web|Creator 1080p60|MP4|H.264|AAC stereo|High|Medium|Large|||
-# Web|Creator 720p60|MP4|H.264|AAC stereo|High|Medium|Large|||
-# Web|Social 25 MB 30 Seconds 1080p60|MP4|H.264|AAC stereo|Depends on source|Medium|25 MB or less|||
-# Web|Social 25 MB 1 Minute 720p60|MP4|H.264|AAC stereo|Depends on source|Medium|25 MB or less|||
-# Web|Social 25 MB 2 Minutes 540p60|MP4|H.264|AAC stereo|Depends on source|Medium|25 MB or less|||
-# Web|Social 25 MB 5 Minutes 360p60|MP4|H.264|AAC stereo|Depends on source|Medium|25 MB or less|||
-# Devices|Amazon Fire 2160p60 4K HEVC Surround|MP4|H.265|AAC stereo; Dolby Digital (AC-3)||Slow||||
-# Devices|Amazon Fire 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Amazon Fire 720p30 Surround|MP4|H.264|AAC stereo||Medium||||
-# Devices|Android 1080p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Android 720p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Android 576p25|MP4|H.264|AAC stereo||Medium||||
-# Devices|Android 480p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Apple 2160p60 4K HEVC Surround|MP4|H.265|AAC stereo; Dolby Digital (AC-3)||Slow||||
-# Devices|Apple 1080p60 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Apple 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Apple 720p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Apple 540p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Chromecast 2160p60 4K HEVC Surround|MP4|H.265|AAC stereo; Dolby Digital (AC-3)||Slow||||
-# Devices|Chromecast 1080p60 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Chromecast 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Playstation 2160p60 4K Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Slow||||
-# Devices|Playstation 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Playstation 720p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Playstation 540p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Roku 2160p60 4K HEVC Surround|MKV|H.265|AAC stereo; AAC, Dolby Digital (AC-3), Dolby Digital Plus (E-AC-3), DTS, or MP3||Slow||||
-# Devices|Roku 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Roku 720p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Devices|Roku 576p25|MP4|H.264|AAC stereo||Medium||||
-# Devices|Roku 480p30|MP4|H.264|AAC stereo||Medium||||
-# Devices|Xbox 1080p30 Surround|MP4|H.264|AAC stereo; Dolby Digital (AC-3)||Medium||||
-# Matroska|AV1 MKV 2160p60 4K|MKV|AV1|AAC stereo||Slow||||
-# Matroska|H.265 MKV 2160p60 4K|MKV|H.265|AAC stereo||Slow||||
-# Matroska|H.265 MKV 1080p30|MKV|H.265|AAC stereo||Slow||||
-# Matroska|H.265 MKV 720p30|MKV|H.265|AAC stereo||Slow||||
-# Matroska|H.265 MKV 576p25|MKV|H.265|AAC stereo||Slow||||
-# Matroska|H.265 MKV 480p30|MKV|H.265|AAC stereo||Slow||||
-# Matroska|H.264 MKV 2160p60 4K|MKV|H.264|AAC stereo||Standard||||
-# Matroska|H.264 MKV 1080p30|MKV|H.264|AAC stereo||Standard||||
-# Matroska|H.264 MKV 720p30|MKV|H.264|AAC stereo||Standard||||
-# Matroska|H.264 MKV 576p25|MKV|H.264|AAC stereo||Standard||||
-# Matroska|H.264 MKV 480p30|MKV|H.264|AAC stereo||Standard||||
-# Matroska|VP9 MKV 2160p60 4K|MKV|VP9|Opus stereo||Ultra slow||||
-# Matroska|VP9 MKV 1080p30|MKV|VP9|Opus stereo||Ultra slow||||
-# Matroska|VP9 MKV 720p30|MKV|VP9|Opus stereo||Ultra slow||||
-# Matroska|VP9 MKV 576p25|MKV|VP9|Opus stereo||Ultra slow||||
-# Matroska|VP9 MKV 480p30|MKV|VP9|Opus stereo||Ultra slow||||
-# Hardware|AV1 QSV 2160p60 4K|MP4|AV1|AAC stereo||Very Fast||||
-# Hardware|H.265 NVENC 2160p60 4K|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 NVENC 1080p60|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 QSV 2160p60 4K|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 QSV 1080p60|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 VCN 2160p60 4K|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 VCN 1080p60|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 MF 2160p60 4K|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 MF 1080p60|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 Apple VideoToolbox 2160p60 4K|MP4|H.265|AAC stereo||Very Fast||||
-# Hardware|H.265 Apple VideoToolbox 1080p60|MP4|H.265|AAC stereo||Very Fast||||
-# Production|Production Max|MP4|H.264|AAC stereo|Max Master|Depends on source|Gigantic|Unlimited|I/P|45669
-# Production|Production Standard|MP4|H.264|AAC stereo|Standard Master|Depends on source|Huge|Unlimited|I/P|45669
-# Production|Production Proxy 1080p|MP4|H.264|AAC stereo|Proxy|Fast|Average|¼ 2160p 4K|Intra-only|1
-# Production|Production Proxy 540p|MP4|H.264|AAC stereo|Proxy|Very Fast|Small|¼ 1080p HD|Intra-only|1
